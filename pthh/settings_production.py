a226a2c524a21e62828765bad45f2c21c58e38f5
Sebastian Schulz <ses.schulz+bitbucket@gmail.com>
2016-02-06 19:22:34 +0100

    Config for assets

--- pthh/settings_production.py
+++ pthh/settings_production.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -87,7 +86,7 @@ TEMPLATE_DIRS = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', '..', 'pthh_static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static', 'static_dirs'),
